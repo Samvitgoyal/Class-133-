f231c910db0a8d85cd1003f92c162c085077bba5
Samvitgoyal <76777983+Samvitgoyal@users.noreply.github.com>
2022-02-22 19:48:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+var img="";
+status1="";
+objects= [];
+
+
+function setup(){
+canvas=createCanvas(600,400);
+canvas.center();
+objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+document.getElementById("status").innerHTML="Status: Detecting objects";
+
+
+}
+
+function preload(){
+img=loadImage("dog_cat.jpg");
+
+}
+
+function draw(){
+image(img,0,0,600,400);
+if(status1 !=""){
+    for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="status: objectsdetected";
+        fill("#11031a");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+        noFill();
+        stroke("#3b065c");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+
+}
+
+
+
+}
+
+function modelLoaded(){
+    console.log("Modelloaded");
+    status1=true;
+ objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+if(error){
+    console.log(error);
+    }
+   else{
+    console.log(results);
+    objects=results;
+    
+   }
+}
